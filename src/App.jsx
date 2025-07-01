d66639a76164d1db788137ee903c2a5241382ca6
Rawda Ahmed <rawdaaa100@gmail.com>
2025-07-01 22:03:53 +0300

    landaing and payment ui

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,20 @@
 import "./App.css";
 import { Toaster } from "react-hot-toast";
-
+import LandingPage from './pages/landing_page'; 
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HelmetProvider } from "react-helmet-async";
+import PaymentPage from "./pages/payment_page";
 function App() {
   return (
-    <>
+    <HelmetProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<LandingPage />} />
+          <Route path="/payment" element={<PaymentPage />} />
+        </Routes>
+      </BrowserRouter>
       <Toaster />
-    </>
+    </HelmetProvider>
   );
 }
 
