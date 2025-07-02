54fa5673e432193803f58fc1330e1e1e31cb9528
محمد فتحي صلاح <130288085+mohamedfathi0121@users.noreply.github.com>
2025-07-02 13:05:25 +0300

    remove react helmet async

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,25 @@
 import "./App.css";
 import { Toaster } from "react-hot-toast";
 
-import LandingPage from './pages/landing_page'; 
+import LandingPage from "./pages/landing_page";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { HelmetProvider } from "react-helmet-async";
 import PaymentPage from "./pages/payment_page";
+import Layout from "./components/Layout";
+import TripDetails from "./pages/TripDetails";
 function App() {
   return (
-    <HelmetProvider>
+    <>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<LandingPage />} />
-          <Route path="/payment" element={<PaymentPage />} />
+          <Route path="/" element={<Layout />}>
+            <Route index element={<LandingPage />} />
+            <Route path="/trip-details" element={<TripDetails />} />
+            <Route path="/payment" element={<PaymentPage />} />
+          </Route>
         </Routes>
       </BrowserRouter>
       <Toaster />
-    </HelmetProvider>
+    </>
   );
 }
 
