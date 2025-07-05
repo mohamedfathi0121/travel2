6d017cc18f97401003fe2962e748fcc4ab6d5356
Romandaibrahim <romandaibrahim9@gmail.com>
2025-07-05 15:45:18 +0300

    convert
    convert from arabic to english and used dark mode

--- src/components/TripDeatailsComponents/NotIncludedItems.jsx
+++ src/components/TripDeatailsComponents/NotIncludedItems.jsx
@@ -1,10 +1,10 @@
 export default function PriceNotIncludes() {
   return (
-    <section className="bg-gray-50 p-4 rounded-lg shadow-sm mb-4 text-right">
-      <h2 className="text-xl font-semibold text-blue-800 mb-2">السعر لا يشمل:</h2>
-      <ul className="list-disc pr-6 ">
-        <li>المشروبات الغازية</li>
-        <li>أي خدمات إضافية داخل الفندق</li>
+    <section className="bg-background p-4 rounded-lg shadow-sm mb-4 text-left">
+      <h2 className="text-xl font-semibold text-text-primary  mb-2">Price Does Not Include:</h2>
+      <ul className="list-disc pl-6 text-text-hard-secondary">
+        <li>Soft drinks</li>
+        <li>Any additional services inside the hotel</li>
       </ul>
     </section>
   );
